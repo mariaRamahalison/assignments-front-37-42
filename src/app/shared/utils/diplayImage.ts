bbd99e31d9de1503bb91a9d9d74b1e7e33b96de5
AndyFaniry <ramarosonandyfaniry@gmail.com>
2023-06-21 01:23:31 +0300

    name

--- src/app/shared/utils/diplayImage.ts
+++ src/app/shared/utils/diplayImage.ts
@@ -2,7 +2,7 @@ import { environment } from "./utils";
 const uri_api = environment.api_url; 
 export const getPhotoUtil = ((name)=>{
     return new Promise<string>((resolve, reject) => {
-        if(name !== ""){
+        if(name && name !== ""){
             resolve(`${uri_api}/photo/${name}`);
           }else{
             resolve(`${uri_api}/photo/1687299429792-200258151.jpg`);
@@ -11,7 +11,7 @@ export const getPhotoUtil = ((name)=>{
 });
 
 export const getPhotoUtilBis = ((name)=>{
-  if(name !== ""){
+  if(name && name !== ""){
     return(`${uri_api}/photo/${name}`);
   }else{
     return(`${uri_api}/photo/1687299429792-200258151.jpg`);
